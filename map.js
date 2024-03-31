5f42a4adaf00b9fc9edcaa66ef7393dd89ec665e
Ankush51620 <164299015+Ankush51620@users.noreply.github.com>
2024-03-31 15:58:27 +0530

    modified

---
+++ map.js
@@ -0,0 +1,230 @@
+const indianStatesAndUTsTourism = [
+    {
+        title: "Andaman Nicobar",
+        description: "Known for pristine beaches, vibrant marine life, and water sports like scuba diving and snorkeling."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Andhra Pradesh",
+        description: "Known for historical monuments, serene beaches of Vishakhapatnam, and ancient temples of Tirupati."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Arunachal Pradesh",
+        description: "Blessed with breathtaking landscapes, lush greenery, vibrant tribal culture, and adventurous activities like trekking and river rafting."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Assam",
+        description: "Renowned for tea gardens, wildlife sanctuaries, the charm of the Brahmaputra river, and vibrant festivals."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Bihar",
+        description: "Rich in history and heritage sites like Bodh Gaya, Nalanda, and wildlife sanctuaries like Valmiki National Park."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Chandigarh",
+        description: "Known for its modern architecture, vibrant nightlife, and cultural events."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Chhattisgarh",
+        description: "Known for ancient caves like Bhimbetka, vibrant tribal culture, and picturesque landscapes."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Daman and Diu",
+        description: "Famous for its serene beaches, colonial architecture, and vibrant culture."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Delhi",
+        description: "The capital city, known for historical landmarks like Red Fort, vibrant markets, and diverse culinary scene."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Dadra and Nagar Haveli",
+        description: "Famous for its serene beaches, colonial architecture, and vibrant culture."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Goa",
+        description: "Famous for its pristine beaches, vibrant nightlife, Portuguese heritage, and water sports."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Gujarat",
+        description: "Home to cultural landmarks like Sabarmati Ashram, vibrant festivals like Navratri, and the stunning Rann of Kutch."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Himachal Pradesh",
+        description: "Renowned for scenic hill stations like Shimla, Manali, adventure sports, and spiritual sites like Dharamshala."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Haryana",
+        description: "Known for its historical sites like Kurukshetra, Badkhal Lake, and the vibrant Surajkund Crafts Mela."
+        ,img:"ooty.jpeg"
+    },
+    
+    
+    {
+        title: "Jharkhand",
+        description: "Famous for its lush forests, waterfalls, and wildlife sanctuaries like Betla National Park."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Jammu and Kashmir",
+        description: "Famous for its picturesque landscapes, rich cultural heritage, and adventure tourism opportunities like trekking and skiing."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Karnataka",
+        description: "Home to historical sites like Hampi, wildlife sanctuaries, hill stations like Coorg, and IT hub Bengaluru."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Kerala",
+        description: "Known as 'God's Own Country' for its backwaters, hill stations like Munnar, and cultural richness."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Lakshadweep",
+        description: "Famous for its pristine coral reefs, crystal-clear waters, and water sports like snorkeling and scuba diving."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Maharashtra",
+        description: "Home to Mumbai, Bollywood, historical sites like Ajanta Ellora caves, and scenic hill stations like Lonavala."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Meghalaya",
+        description: "Famous for its living root bridges, scenic waterfalls, and adventurous activities like caving and trekking."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Manipur",
+        description: "Renowned for its scenic landscapes, cultural richness, and vibrant festivals like Sangai Festival."
+        ,img:"ooty.jpeg"
+    },
+   
+    {
+        title: "Madhya Pradesh",
+        description: "Famous for wildlife sanctuaries like Kanha National Park, historical sites like Khajuraho, and ancient caves."
+        ,img:"ooty.jpeg"
+    },
+    
+    
+    {
+        title: "Mizoram",
+        description: "Known for its picturesque landscapes, vibrant festivals, and rich tribal culture."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Nagaland",
+        description: "Renowned for its Hornbill Festival, scenic landscapes, and vibrant tribal culture."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Odisha",
+        description: "Famous for its temples like Konark Sun Temple, pristine beaches, and vibrant tribal culture."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Punjab",
+        description: "Known for its rich history, religious sites like Golden Temple, and vibrant culture."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Puducherry",
+        description: "Known for its colonial architecture, serene beaches, and vibrant French culture."
+        ,img:"ooty.jpeg"
+    },
+    
+    {
+        title: "Rajasthan",
+        description: "Renowned for its forts and palaces like Jaipur's Hawa Mahal, Thar Desert, and vibrant culture."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Sikkim",
+        description: "Famous for its scenic beauty, Buddhist monasteries like Rumtek, and adventure tourism."
+        ,img:"ooty.jpeg"
+    },
+    
+    {
+        title: "Telangana",
+        description: "Known for its IT hub Hyderabad, historic sites like Golconda Fort, and vibrant culture."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Tamil Nadu",
+        description: "Home to cultural landmarks like Madurai Meenakshi Temple, hill stations like Ooty, and historic sites."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Tripura",
+        description: "Renowned for its palaces, scenic beauty, and cultural richness."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Uttar Pradesh",
+        description: "Famous for religious sites like Varanasi, Agra's Taj Mahal, and cultural festivals."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "Uttarakhand",
+        description: "Home to the Himalayas, pilgrimage sites like Haridwar, and scenic hill stations like Nainital."
+        ,img:"ooty.jpeg"
+    },
+    {
+        title: "West Bengal",
+        description: "Known as the 'Cultural Capital of India', offering a blend of history, art, culinary delights, and the serene beauty of Darjeeling hills."
+        ,img:"ooty.jpeg"
+    }
+];
+
+console.log(indianStatesAndUTsTourism.length);
+
+const stateElements = document.querySelectorAll('path'); // Assuming you've used div elements to represent states
+stateElements.forEach(element => {
+    element.addEventListener('click', handleStateClick);
+});
+
+// stateElements[20].path.fill="#fa9121";
+// console.log(stateElements);
+// const first=document.getElementById('20');
+// let firstindex=Number.parseInt(first);
+// const  head=first.innerHTML=indianStatesAndUTsTourism[19].title;
+// const  content=first.innerHTML=indianStatesAndUTsTourism[19].description;
+// const img=document.getElementById('state-image');
+// img.src="./states_ut/" + 20+ ".jpg";
+
+
+
+const  head=document.getElementById('head').innerHTML=indianStatesAndUTsTourism[20-1].title;
+    const  content=document.getElementById('content').innerHTML=indianStatesAndUTsTourism[20-1].description;
+    const img=document.getElementById('state-image');
+    
+    img.src="./states_ut/" + "20"+ ".jpg";
+//    const col = document.getElementById('20').color='#fa9121';
+//    console.log(col); 
+
+
+function handleStateClick(event) {
+    const clickedId = event.target.id; // Get the ID of the clicked element
+    let index=Number.parseInt(clickedId);
+    console.log(index);
+    console.log(indianStatesAndUTsTourism[index-1].title);
+    console.log(indianStatesAndUTsTourism[index-1].description);
+    const  head=document.getElementById('head').innerHTML=indianStatesAndUTsTourism[index-1].title;
+    const  content=document.getElementById('content').innerHTML=indianStatesAndUTsTourism[index-1].description;
+    const img=document.getElementById('state-image');
+    
+    img.src="./states_ut/" + clickedId+ ".jpg";
+
+}
--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 gsap.to(".nav-bar",{
     backgroundColor:"#141414",
     height:"90px",
-    duration:0.7,
+    duration:0.5,
     scrollTrigger:{
-        trigger:".header",
-        scroller:"body",
+        // trigger:".header",
+        // scroller:"body",
         
-        start:"top -10%",
-        end:"top -11%",
-        scrub:1,
+        // start:"top -10%",
+        // end:"top -11%",
+        // scrub:1,
+        trigger: "#page2", // Change this to the appropriate trigger element for the second section
+      scroller: "body",
+      start: "top center", // Adjust start and end positions as needed
+      end: "bottom center",
+      scrub: true
     }
 })
 
@@ -32,22 +37,70 @@ gsap.to('.char',{
     delay:0.2,
     duration:0.1
 })
-gsap.to('#main',{
-  backgroundColor:"#FEFAE0",
-  scrollTrigger:{
-    trigger:"#main",
-    scroller:"body",
-    
-    start:"top -10%",
-    end:"top -40%",
-    scrub:2,
+
+
+
+// Assuming that the lenis and SplitType sections are functioning correctly,
+// I'll focus on correcting the GSAP scrollTrigger part of the code.
+
+// gsap.to(".nav-bar", {
+//   backgroundColor:"#141414",
+//   height:"90px",
+//   duration:0.5,
+//   scrollTrigger: {
+//       trigger: ".section-two", // Change this to the appropriate trigger element for the second section
+//       scroller: "body",
+//       start: "top center", // Adjust start and end positions as needed
+//       end: "bottom center",
+//       scrub: true
+//   }
+// });
+
+// const lenis = new Lenis();
+
+// lenis.on('scroll', (e) => {
+//   console.log(e);
+// });
+
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+
+// requestAnimationFrame(raf);
+
+// const myText = new SplitType('#my-text');
+
+// gsap.to('.char', {
+//   y: 0,
+//   stagger: 0.05,
+//   delay: 0.2,
+//   duration: 0.1
+// });
+
+
+
+
+
+// js - carouel***********
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const carouselWrapper = document.querySelector('.carousel-wrapper');
+  const slides = document.querySelector('.carousel-slide');
+  const slideWidth = document.querySelector('.carousel-content').offsetWidth;
+  const numSlides = slides.children.length;
+  const carouselWidth = slideWidth * numSlides;
+
+  let counter = 0;
+
+  function slide() {
+    if (counter+slideWidth >= carouselWidth) {
+      counter = 0;
+    }
+    carouselWrapper.style.transform = `translateX(${-counter}px)`;
+    counter+=slideWidth+40;
   }
 
-})
-
-
-
-
-
-
-
+  setInterval(slide, 2500); // Adjust the interval as needed (2000ms = 2 seconds)
+});
